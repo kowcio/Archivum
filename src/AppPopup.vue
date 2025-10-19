bf5421610b932fca4232590d7d8edfd80a46dbb7
Ave <kowcio2@gmail.com>
2025-10-19 14:18:20 +0200

    12

--- src/AppPopup.vue
+++ src/AppPopup.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>Popup Mounted!</h1>
   </div>
-  <!-- <div id="tab-list" style="">
+  <div id="tab-list" style="">
     <h1>Tabs</h1>
   </div>
   <div>
@@ -12,33 +12,31 @@
       </li>
     </ul>
     <button @click="loadTabs()" class="button">Get tabs!</button>
-  </div> -->
+  </div>
 </template>
 
 <script setup lang="ts">
-import 'chrome'
-import 'chrome-types'
-// import { computed, defineExpose, onMounted, ref } from 'vue'
-import { onMounted } from 'vue'
-// import browser from 'webextension-polyfill'
-// import type { Tabs } from 'webextension-polyfill'
+import { onMounted, ref } from 'vue'
+// import { onMounted } from 'vue'
+import browser from 'webextension-polyfill'
+import type { Tabs } from 'webextension-polyfill'
 
 // const exposedValue = computed(() => {
 //   return 'exposed value'
 // })
 
-// const tabs = ref<Tabs.Tab[]>([])
-// async function loadTabs(): Promise<void> {
-//   try {
-//     const fetchedTabs: Tabs.Tab[] = await browser.tabs.query({ currentWindow: true })
-//     fetchedTabs.forEach((tab: Tabs.Tab) => {
-//       console.log(tab)
-//       tabs.value.push(tab)
-//     })
-//   } catch (error) {
-//     console.error(error)
-//   }
-// }
+const tabs = ref<Tabs.Tab[]>([])
+async function loadTabs(): Promise<void> {
+  try {
+    const fetchedTabs: Tabs.Tab[] = await browser.tabs.query({ currentWindow: true })
+    fetchedTabs.forEach((tab: Tabs.Tab) => {
+      console.log(tab)
+      tabs.value.push(tab)
+    })
+  } catch (error) {
+    console.error(error)
+  }
+}
 
 // defineExpose({ exposedValue, tabs })
 
